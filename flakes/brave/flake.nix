cc7892b97b5ee010c3f7436fe0e03048721c7b2d
lippiece <github@lippiece.anonaddy.me>
2024-08-13 22:25:00 +0200

    feat (flakes): add brave flake

---
+++ flakes/brave/flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "A flake that packages the latest nightly release of Brave Browser";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        archiveSha256 = "sha256-E7geCfjfeKm0Ee4YJntY3UjIRnbjryEeYVIrvTBJw/I=";
+
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        owner = "brave";
+        repo = "brave-browser";
+
+        latestRelease = builtins.fromJSON (builtins.readFile (pkgs.fetchurl {
+          url = "https://api.github.com/repos/${owner}/${repo}/releases";
+          sha256 = "sha256-KNFDGOz8Yzb0JdjjPWuTC9QOvnwWGfZ0iFUfMeTjnQE=";
+        }));
+
+        latestNightly = builtins.head (builtins.filter
+          (
+            release:
+              pkgs.lib.hasPrefix "Nightly" release.name
+          )
+          latestRelease);
+
+        version = builtins.elemAt (builtins.match "Nightly v([0-9.]+) .*" latestNightly.name) 0;
+
+        asset = builtins.head (builtins.filter
+          (
+            asset:
+              pkgs.lib.hasSuffix "linux-amd64.zip" asset.name
+          )
+          latestNightly.assets);
+      in {
+        packages.brave-browser-nightly = pkgs.stdenv.mkDerivation {
+          pname = "brave-browser-nightly";
+          inherit version;
+
+          src = pkgs.fetchzip {
+            stripRoot = false;
+            url = asset.browser_download_url;
+            sha256 = archiveSha256;
+          };
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r * $out/
+            ln -s $out/brave $out/bin/brave
+
+            mkdir -p $out/share/applications
+            cat <<EOF >$out/share/applications/brave-browser-nightly.desktop
+            [Desktop Entry]
+            Name=brave-browser-nightly
+            Exec=$out/bin/brave %f
+            Type=Application
+            EOF
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Brave Browser Nightly Build";
+            homepage = "https://brave.com";
+            license = licenses.mpl20;
+            platforms = ["x86_64-linux"];
+            maintainers = with maintainers; [];
+          };
+        };
+
+        packages.default = self.packages.${system}.brave-browser-nightly;
+      }
+    );
+}
---
+++ flakes/brave/update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Extract the path to the nix file (change this if necessary)
+nix_file="flake.nix"
+
+# Replace the old SHA256 with a placeholder
+placeholder="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+sed -i "s/archiveSha256 = \".*\";/archiveSha256 = \"$placeholder\";/" $nix_file
+
+# Run nix build to intentionally get the hash mismatch error
+output=$(nix build 2>&1)
+
+# Extract the received SHA256 from the error message
+new_hash=$(echo "$output" | grep 'got:' | awk '{print $2}')
+echo $new_hash
+
+# Replace the placeholder in the nix file with the new one
+if [[ -n $new_hash ]]; then
+  sed -i "s/archiveSha256 = \".*\";/archiveSha256 = \"$new_hash\";/" $nix_file
+  echo "Updated sha256 to $new_hash in $nix_file"
+else
+  echo "Failed to find a new SHA256 hash."
+fi
