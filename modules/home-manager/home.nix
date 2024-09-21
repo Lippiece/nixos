d933cc1384b521aeea31ac7523174852bee96cf3
lippiece <github@lippiece.anonaddy.me>
2024-09-22 01:30:19 +0200

    147 current  2024-09-22 01:30:14  24.11.20240920.a1d9266  6.11.0                          *

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -198,6 +198,7 @@
         url = "https://habr.com/ru/rss/feed/d6e1aa020767fe5324b423fc403b5751?fl=en%2Cru&rating=25&types%5B%5D=article&types%5B%5D=post";
       }
       {url = "https://bun.sh/rss.xml";}
+      {url = "https://kde.org/index.xml";}
       {url = "http://cumulonimbus:4002/rss/test";}
     ];
   };
