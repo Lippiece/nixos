b00a2d4eb6d1113f4a69a06263ebd29bad519106
lippiece <github@lippiece.anonaddy.me>
2024-11-02 15:55:20 +0200

    feat(packages): add catapult

---
+++ packages/catapult/default.nix
@@ -0,0 +1,25 @@
+{pkgs ? import <nixpkgs> {}}: let
+  inherit (pkgs) buildFHSUserEnv;
+in
+  buildFHSUserEnv {
+    name = "catapult-linux-x64-24.07a-fhs";
+    targetPkgs = pkgs:
+      with pkgs; [
+        xorg.libXcursor.out
+        xorg.libXi.out
+        xorg.libXinerama.out
+        xorg.libXrender.out
+        libGL.out
+        xorg.libX11.out
+        xorg.libXext.out
+        xorg.libXrandr.out
+
+        SDL2.out
+        SDL2_image_2_0.out
+        SDL2_mixer_2_0.out
+        SDL2_ttf.out
+        libgcc.lib
+        zlib.out
+      ];
+    runScript = "/home/lippiece/.config/nixos/packages/catapult/catapult-linux-x64-24.07a";
+  }
