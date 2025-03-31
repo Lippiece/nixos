523111abbd77ce791e03423c28815ca6931250c6
lippiece <github@lippiece.anonaddy.me>
2025-03-31 19:18:02 +0200

    293 current  2025-03-31 19:17:59  25.05.20250330.52faf48  6.14.0                          *

--- update.sh
+++ update.sh
@@ -5,24 +5,27 @@ echo Building (dirs).
 # Extract the path to the nix file (change this if necessary)
 set nix_file "flake.nix"
 
-update-nix-fetchgit -v flake.nix
+# update-nix-fetchgit -v flake.nix
 
 # Run nix build to intentionally get the hash mismatch error
 set output (nix build 2>&1)
 
 # Extract the received SHA256 from the error message
-set old_hash (echo "$output" | grep -Eo "specified:.+" | awk '{print $2}' | string escape)
-set new_hash (echo "$output" | grep -Eo "got:.+" | awk '{print $2}' | string escape)
+set hashes (echo "$output" | ag -o 'sha256.\K\S+')
+set old_hash (echo "$hashes" | awk '{print $1}')
+set new_hash (echo "$hashes" | awk '{print $2}')
 
 # Replace the placeholder in the nix file with the new one
-if echo $new_hash | grep "sha"
+if echo "$output" | ag "mismatch"
     sed -i "s#$old_hash#$new_hash#" $nix_file
-    echo "Updated $old_hash to $new_hash in $nix_file."
+    echo "Updated $old_hash"
+    echo "to $new_hash"
+    echo "in $nix_file."
 
     ./update.sh
 else
     echo "Failed to find a new SHA256 hash,"
-    echo "or the packages was successfully built."
+    echo "or the package was successfully built."
     echo $output
 end
-notify-send "Brave flake concluded."
+notify-send "$(dirs) flake concluded."
