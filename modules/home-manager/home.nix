4574580fa62ffd5d92c32a91c6a5823b280eac19
lippiece <github@lippiece.anonaddy.me>
2026-01-14 14:31:21 +0200

    35          2026-01-14 14:31:17  26.05.20260111.ffbc9f8  6.18.4  Unknown                 []              True

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -142,7 +142,7 @@ in {
     '';
 
     ".config/chromium/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
-    ".config/BraveSoftware/Brave-Browser/NativeMessagingHosts/org.kde.plasma.browser_integration".source = "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+    ".config/BraveSoftware/Brave-Browser/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
