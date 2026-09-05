8aa796d838f0a8c414306f4a82113310b0975ad7
lippiece <github@lippiece.anonaddy.me>
2026-09-05 23:08:12 +0700

    184         2026-09-05 23:08:06  26.11.20260831.34ab990  7.2.2   Unknown                 []              True

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -82,10 +82,10 @@ in {
         # upstream = [ "tls://dns.adguard.com" ];
         # DNS over HTTPS upstream
         upstream = [
-          # "quic://lipguard.lippiece.ru"
-          "tls://1.1.1.1"
-          "tls://8.8.8.8"
-          "tls://9.9.9.9"
+          "quic://lipguard.lippiece.ru"
+          # "tls://1.1.1.1"
+          # "tls://8.8.8.8"
+          # "tls://9.9.9.9"
         ];
 
         fallback = [
